3373b4139d8bd9e79b77f18486e0ba7fb298b41b
Navya Agarwal <navyaagarwalofficial@gmail.com>
2026-02-27 16:18:05 +0530

    [Navya] Added Use Case 3 Palindrome Checker

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,18 +1,24 @@
 public class PalindromeCheckerApp{
     public static void main(String[] args) {
+        String word = "racecar";
+        char[] chars = word.toCharArray();
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = chars.length - 1;
 
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
 
-                String original = "level";
-                String reversed = "";
-
-                for (int i = original.length() - 1; i >= 0; i--) {
-                    reversed = reversed + original.charAt(i);
-                }
-
-                if (original.equals(reversed)) {
-                    System.out.println("The given string \"" + original + "\" is a Palindrome.");
-                } else {
-                    System.out.println("The given string \"" + original + "\" is not a Palindrome.");
-                }
+        if (isPalindrome) {
+            System.out.println("The given string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The given string \"" + word + "\" is not a Palindrome.");
+        }
             }
         }
