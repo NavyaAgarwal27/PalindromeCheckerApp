228aa90199cd590889bcff632ae359e745651fe4
Navya Agarwal <navyaagarwalofficial@gmail.com>
2026-02-27 15:45:28 +0530

    [Navya] added Use Case 2 Palindrome Checker

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,16 +1,16 @@
 public class PalindromeCheckerApp{
     public static void main(String[] args) {
+        String word = "madam";
+        String reversed = "";
 
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
 
-        String version = "Version 1.0";
-
-
-
-        System.out.println(version);
-
-        System.out.println("Welcome to the Palindrome Checker Application");
-        System.out.println(version);
-        System.out.println("Application initialized successfully.");
-
+        if (word.equals(reversed)) {
+            System.out.println("The given string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The given string \"" + word + "\" is not a Palindrome.");
+        }
     }
 }
