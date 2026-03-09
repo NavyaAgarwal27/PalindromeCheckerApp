285af6c267d4579f491739248f76ffdf044726e3
Navya Agarwal <navyaagarwalofficial@gmail.com>
2026-03-09 14:37:05 +0530

    [Navya] Added Use Case 8 Palindromec Checker

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,27 +1,72 @@
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class PalindromeCheckerApp {
+
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
     public static void main(String[] args) {
-        String word = "level";
-        Deque<Character> deque = new LinkedList<>();
-
-        for (int i = 0; i < word.length(); i++) {
-            deque.addLast(word.charAt(i));
-        }
-
-        boolean isPalindrome = true;
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
+        String word = "madam";
+        Node head = createLinkedList(word);
+        boolean isPalindrome = checkPalindrome(head);
         if (isPalindrome) {
             System.out.println("The given string \"" + word + "\" is a Palindrome.");
         } else {
             System.out.println("The given string \"" + word + "\" is not a Palindrome.");
         }
+    }
+
+    private static Node createLinkedList(String word) {
+        Node head = new Node(word.charAt(0));
+        Node current = head;
+        for (int i = 1; i < word.length(); i++) {
+            current.next = new Node(word.charAt(i));
+            current = current.next;
+        }
+        return head;
+    }
+
+    private static boolean checkPalindrome(Node head) {
+        if (head == null || head.next == null) return true;
+
+        Node slow = head;
+        Node fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverseList(slow.next);
+        Node firstHalf = head;
+
+        Node tempSecond = secondHalf;
+        boolean isPalindrome = true;
+        while (tempSecond != null) {
+            if (firstHalf.data != tempSecond.data) {
+                isPalindrome = false;
+                break;
+            }
+            firstHalf = firstHalf.next;
+            tempSecond = tempSecond.next;
+        }
+
+        slow.next = reverseList(secondHalf);
+        return isPalindrome;
+    }
+
+    private static Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
     }
 }
