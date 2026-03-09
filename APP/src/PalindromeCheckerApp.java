21f6c5cfffcb8252a1aa8b90e9fc653f41c6fb94
Navya Agarwal <navyaagarwalofficial@gmail.com>
2026-03-09 15:06:36 +0530

    [Navya] Added Use Case 13 Palindrome Chceker

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,16 +1,46 @@
-public class PalindromeCheckerApp{
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String word = "noon";
-        if (isPalindrome(word, 0, word.length() - 1)) {
-            System.out.println("The given string \"" + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("The given string \"" + word + "\" is not a Palindrome.");
-        }
+        String word = "amanaplanacanalpanama";
+
+        long start, end;
+
+        start = System.nanoTime();
+        boolean result1 = checkPalindromeWithStack(word);
+        end = System.nanoTime();
+        System.out.println("Stack method: " + result1 + " | Time: " + (end - start) + " ns");
+
+        start = System.nanoTime();
+        boolean result2 = checkPalindromeWithDeque(word);
+        end = System.nanoTime();
+        System.out.println("Deque method: " + result2 + " | Time: " + (end - start) + " ns");
+
+        start = System.nanoTime();
+        boolean result3 = checkPalindromeWithStringReverse(word);
+        end = System.nanoTime();
+        System.out.println("String reverse method: " + result3 + " | Time: " + (end - start) + " ns");
     }
 
-    private static boolean isPalindrome(String s, int start, int end) {
-        if (start >= end) return true;
-        if (s.charAt(start) != s.charAt(end)) return false;
-        return isPalindrome(s, start + 1, end - 1);
+    private static boolean checkPalindromeWithStack(String word) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : word.toCharArray()) stack.push(c);
+        for (char c : word.toCharArray()) if (c != stack.pop()) return false;
+        return true;
+    }
+
+    private static boolean checkPalindromeWithDeque(String word) {
+        Deque<Character> deque = new LinkedList<>();
+        for (char c : word.toCharArray()) deque.addLast(c);
+        while (deque.size() > 1) if (!deque.removeFirst().equals(deque.removeLast())) return false;
+        return true;
+    }
+
+    private static boolean checkPalindromeWithStringReverse(String word) {
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) reversed += word.charAt(i);
+        return word.equals(reversed);
     }
 }
